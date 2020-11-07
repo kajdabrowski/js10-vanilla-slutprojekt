0243c7445ce920ceaf440b712e395d21e0c2310f
viktor-okomdal <70326355+viktor-okomdal@users.noreply.github.com>
2020-11-07 15:05:07 +0100

    javascript fetch

--- main.js
+++ main.js
@@ -12,18 +12,64 @@ for( let button of buttons){
     })
 }
 
+let randomBeer = "https://api.punkapi.com/v2/beers/random"
 
-const beerbutton = document.querySelector(".see-more > button")
+let getData = async requestedData => {
+    let request = await fetch(requestedData)
+    let data  = await request.json()
+    return data
+    
+}
+
+let data = "https://api.punkapi.com/v2/beers/random"
+
+console.log(getData(randomBeer));
 
 
-    beerbutton.addEventListener("click", () => {
-        document.querySelectorAll(".main-page > section").forEach(
-            section => section.classList.remove("show"))
+ let beerish = document.querySelector(".beer-name") 
+
+ beerish.innerText = getData(data.name)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const beerbutton = document.querySelector(".see-more > button")
+
+
+//     beerbutton.addEventListener("click", () => {
+//         document.querySelectorAll(".main-page > section").forEach(
+//             section => section.classList.remove("show"))
       
-      const section = document.querySelector("." + beerbutton.innerText)
-      section.classList.add("show")
-    })
+//       const section = document.querySelector("." + beerbutton.innerText)
+//       section.classList.add("show")
+//     })
 
-
-// kajsson
 
