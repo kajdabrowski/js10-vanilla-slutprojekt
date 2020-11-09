6cf21cf5c45d4614d86149ae95ed7d87c7ea5964
viktor-okomdal <70326355+viktor-okomdal@users.noreply.github.com>
2020-11-09 14:32:59 +0100

    Beer image working and see-more button

--- main.js
+++ main.js
@@ -12,31 +12,46 @@ for( let button of buttons){
     })
 }
 
-let randomBeer = "https://api.punkapi.com/v2/beers/random"
+const seeMoreBtn = document.querySelector(".see-more-btn")
+seeMoreBtn.addEventListener("click", () =>{
+   document.querySelector(".Home").classList.remove("show")
 
-async function getData(name) {
+   document.querySelector(".Beer-Info").classList.add("show")
+})
+
+let randomBeerUrl = "https://api.punkapi.com/v2/beers/random"
+
+async function getData() {
     // try to fetch data, if anything goes wrong catch the error and log it.
     try {
-        let request = await fetch(randomBeer)
+        let request = await fetch(randomBeerUrl)
         let data  = await request.json()
-
+      
         return data; //beerName.innerText = data[0].name;  // Ett annat alternativ
         
     } catch(error) {
         console.log('error', error)
     }
 }
-getData().then(data => beerName.innerText = data[0].name)
-getData().then(data => beerImage.innerText = data[0].image_url)
+// let myJsonData = getData();
 
-// let beerImageDOM = document.querySelector("div.beer-image")
-// let beerImg = document.beer-image.createElement("img");
-// beerImg.setAttribute("src", data[0].image_url);
+
+
+
+
+
+// console.log("Json",myJsonData);
+getData().then(data => beerName.innerText = data[0].name)
+getData().then(data => beerImage.src = data[0].image_url)
+
+// // let beerImageDOM = document.querySelector("div.beer-image")
+// // let beerImg = document.beer-image.createElement("img");
+// // beerImg.setAttribute("src", data[0].image_url);
 
 let beerName = document.querySelector(".beer-name") 
 let beerImage = document.querySelector(".beer-image")
 
-console.log('getData', getData())
+// console.log('getData', getData())
 
 
 
